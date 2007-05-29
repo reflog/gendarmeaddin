d9141e717f662e80bbe148819e8214906a7ba31c
reflog <reflog@cc186d6d-3a31-0410-9da0-31d5ccbdc6a6>
2007-05-29 10:33:48 +0000

    tag

---
+++ tags/release-0.5/GendarmeAddin/GendarmeAddin.cs
@@ -0,0 +1,92 @@
+//
+// Copyright (c) Eli Yukelzon - reflog@gmail.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Ide.Gui.Pads;
+using MonoDevelop.Ide.Gui.Content;
+using Gtk;
+using System.Collections.Generic;
+
+
+
+namespace MonoDevelop {
+
+    namespace GendarmeAddin {
+        public enum Commands {
+            TestSingleSolution,
+            TestWholeSolution
+        }
+        
+      class TestSingleSolutioNodeExtension : NodeBuilderExtension
+            {
+                    public override bool CanBuildNode (Type dataType)
+                    {
+                            return typeof (DotNetProject).IsAssignableFrom (dataType);
+                    }
+
+                    public override Type CommandHandlerType {
+                            get { return typeof ( TestSingleSolutionHandler ); }
+                    }
+            }
+
+      class TestWholeSolutioNodeExtension : NodeBuilderExtension
+            {
+                    public override bool CanBuildNode (Type dataType)
+                    {
+                            return typeof (Combine).IsAssignableFrom (dataType);
+                    }
+
+                    public override Type CommandHandlerType {
+                            get { return typeof ( TestWholeSolutionHandler ); }
+                    }
+            }
+
+    public class TestSingleSolutionHandler : NodeCommandHandler {
+                    [CommandHandler (Commands.TestSingleSolution)]
+                    protected void OnTest()
+                    {
+                        MonoDevelop.GendarmePad pad = (MonoDevelop.GendarmePad) IdeApp.Workbench.Pads[typeof(MonoDevelop.GendarmePad)].Content;
+                        IdeApp.Workbench.Pads[typeof(MonoDevelop.GendarmePad)].BringToFront();
+                        (pad.Control as MonoDevelop.GendarmeDisplay).RunTests(CurrentNode.DataItem);
+                    }
+            }
+
+    public class TestWholeSolutionHandler : NodeCommandHandler {
+
+                    [CommandHandler (Commands.TestWholeSolution)]
+                    protected void OnTest()
+                    {
+                        MonoDevelop.GendarmePad pad = (MonoDevelop.GendarmePad) IdeApp.Workbench.Pads[typeof(MonoDevelop.GendarmePad)].Content;
+                        IdeApp.Workbench.Pads[typeof(MonoDevelop.GendarmePad)].BringToFront();
+                        (pad.Control as MonoDevelop.GendarmeDisplay) .RunTests(CurrentNode.DataItem);
+                    }
+            }
+
+    }
+}
---
+++ tags/release-0.5/GendarmeAddin/GendarmeConfig.cs
@@ -0,0 +1,107 @@
+// CopyRight (c) Eli Yukelzon
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
+using System;
+using System.IO;
+using System.Collections;
+
+using MonoDevelop.Core.Properties;
+using MonoDevelop.Core;
+
+using MonoDevelop.Core.Gui.Components;
+using MonoDevelop.Components;
+using MonoDevelop.Core.Gui.Dialogs;
+using Gtk;
+
+namespace MonoDevelop
+{
+	public enum GendarmeGroupingType {
+	    None,
+	    Solution,
+	    Reason,
+	    SolutionAndReason
+	}
+	
+	public partial class GendarmeConfig : Gtk.Bin
+	{
+	    public const string defaultFile = "/usr/lib/gendarme/rules.xml";
+	    public const string defaultSet = "default";
+	    
+		public GendarmeConfig()
+		{
+			this.Build();
+    		edtPath.Text = Runtime.Properties.GetProperty ("GendarmeAddIn.Path", defaultFile);
+    		edtSet.Text = Runtime.Properties.GetProperty ("GendarmeAddIn.Set", defaultSet);
+    		GendarmeGroupingType mode = (GendarmeGroupingType)Runtime.Properties.GetProperty ("GendarmeAddIn.GroupMode", (int)GendarmeGroupingType.None);
+    		rbNone.Active = mode == GendarmeGroupingType.None;
+    		rbSolution.Active = mode == GendarmeGroupingType.Solution;
+    		rbSolutionProb.Active = mode == GendarmeGroupingType.SolutionAndReason;
+    		rbType.Active = mode == GendarmeGroupingType.Reason;
+		}
+		
+		public void Store(){
+    		Runtime.Properties.SetProperty ("GendarmeAddIn.Path", edtPath.Text);
+    		Runtime.Properties.SetProperty ("GendarmeAddIn.Set", edtSet.Text);
+    		GendarmeGroupingType mode = GendarmeGroupingType.None;
+    		if(rbSolution.Active) 
+    		    mode = GendarmeGroupingType.Solution;
+    		else if(rbType.Active) 
+    		    mode = GendarmeGroupingType.Reason;
+    		else if(rbSolutionProb.Active) 
+    		    mode = GendarmeGroupingType.SolutionAndReason;
+    		    
+    		Runtime.Properties.SetProperty ("GendarmeAddIn.GroupMode", (int)mode);
+    		Runtime.Properties.SaveProperties ();
+		}
+		
+        MonoDevelop.Components.FileSelector d = new MonoDevelop.Components.FileSelector
+                ("Select Gendarme Rules File", FileChooserAction.Open);
+        
+		protected virtual void OnBtnOpenClicked(object sender, System.EventArgs e)
+		{		
+    		d.Filter = new FileFilter();
+    		d.Filter.AddMimeType("text/xml");
+    		if (d.Run() == (int)ResponseType.Ok)
+    			  edtPath.Text = d.Filename;
+       		d.Hide();		  
+		}
+
+		
+	}
+	
+	 public class GendarmeConfigPanel : AbstractOptionPanel
+        {
+                GendarmeConfig widget;
+                public override void LoadPanelContents()
+                {
+                        widget = new GendarmeConfig ();
+                        Add (widget);
+                }
+
+                public override bool StorePanelContents()
+                {
+                        widget.Store ();
+                        return true;
+                }
+        }
+}
